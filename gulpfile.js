59b9c2c3205912be95c8d4b27d5175378659e867
Ohoud Alawad <ohoudalawad@gmail.com>
2018-03-21 22:08:47 +0300

    Download tools and write gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var flipper = require('gulp-css-flipper');
+var scssToCss = require('gulp-sass');
+
+gulp.task('connect', function(){
+    connect.server({
+        root: 'build',
+        livereload: true,
+        port:8080
+
+    });
+});
+
+gulp.task('html', function(){
+    return gulp.src('src/*.html')
+    .pipe(gulp.dest('build'))
+    .pipe(connect.reload());
+
+});
+
+gulp.task('js', function(){
+    return gulp.src('src/js/*.js')
+    .pipe(gulp.dest('build/js'))
+    .pipe(connect.reload());
+
+});
+
+gulp.task('images', function(){
+    return gulp.src('src/images/*.*')
+    .pipe(gulp.dest('build/images'))
+    .pipe(connect.reload());
+
+});
+
+gulp.task('fonts', function(){
+    return gulp.src('src/fonts/*.*')
+    .pipe(gulp.dest('build/fonts'))
+    .pipe(connect.reload());
+
+});
+
+gulp.task('css',function(){
+    var flip = gulp.src(['src/css/*.css']).pipe(flipper());
+    return flip
+    .pipe(gulp.dest('build/css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('scss', function(){
+    return gulp.src('src/scss/*.scss')
+    .pipe(scssToCss({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('build/css'));
+
+});
+
+
+gulp.task('build', ['html','css','js','images','fonts', 'scss']);
+
+gulp.task('watch',function(){
+    return gulp.watch('src/**/*',['build']);
+});
+
+gulp.task('default',['build','connect','watch']);
+
+
+    
+
